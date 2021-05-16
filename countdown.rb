f8766e3b783ee57ab22967a99296b4207fc74842
Amy Mangor <aemangor@gmail.com>
2021-05-16 16:24:22 -0400

    commit tree 2a48148cdb555a6f51816151a82aa55ea4f07bf8

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
-end
+def countdown(to_midnight)
+    while to_midnight > 0
+      puts "#{to_midnight} SECOND(S)!"
+      to_midnight -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
+
+  def countdown_with_sleep(to_midnight)
+    while to_midnight > 0
+        puts "#{to_midnight} SECOND(S)!"
+        sleep(1)
+        to_midnight -= 1
+      end
+      "HAPPY NEW YEAR!"
+    end
